c08c2de85fabcd133b62821e5f8b816d2c1a8fc2
yousef2006 <yw0122@gmail.com>
2025-10-27 07:19:33 +0300

    A. Bear and Big Brother

---
+++ A. Bear and Big Brother/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+int main(){
+    int a,b,n=0;
+    cin >> a >> b;
+    do
+    {
+        a*=3;
+        b*=2;
+        n++;
+    } while (a<=b);
+    cout << n ;
+}
